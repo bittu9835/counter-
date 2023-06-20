50a246cef6d9d24b1b7420f3d31a69488fee8d4d
Bittu kumar <bk2671111@gmail.com>
2023-06-20 13:07:35 +0530

    final

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,22 @@
 
 let a = 0;
 
+const decrese = () => {
+    a -= 1;
+    if(a>=0){
+        document.getElementById("output").innerHTML = a;
+    } 
+    if(a<=0){
+        a=0;
+    } 
+};
+
 const increse = () => {
     a += 1;
     document.getElementById("output").innerHTML = a;
 };
 
-const decrese = () => {
-    a -= 1;
-    document.getElementById("output").innerHTML = a;
-};
+
 
 const zero = () => {
     a = 0;
